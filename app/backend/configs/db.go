8b07aa78a33b493c69cec10c05f6f99393f6a097
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-08-01 13:55:47 +0100

    Update

--- app/backend/configs/db.go
+++ app/backend/configs/db.go
@@ -18,15 +18,13 @@ type DB struct {
 }
 
 func ConnectDB() *DB {
-	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
