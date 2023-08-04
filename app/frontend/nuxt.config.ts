9f23118c230cf1ce3ff33ff385902344029af329
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-08-04 17:21:12 +0100

    Modules Update

--- app/frontend/nuxt.config.ts
+++ app/frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-graphql-server"],
   ssr: false,
   css: [
     "@ionic/core/css/core.css",
