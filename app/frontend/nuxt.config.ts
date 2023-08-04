75d68d5037b145c254cc5569ca88f51842452779
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-08-04 21:13:00 +0100

    Update and Fixes

--- app/frontend/nuxt.config.ts
+++ app/frontend/nuxt.config.ts
@@ -1,17 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-graphql-server"],
-  graphqlServer: {
-    url: "/api/graphql",
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', 'nuxt-icon', "@nuxtjs/color-mode", '@nuxt/image-edge'],
+  css: ["@/assets/css/main.css"],
+  image: {
+      domains: ['rickandmortyapi.com']
   },
-  ssr: false,
-  css: [
-    "@ionic/core/css/core.css",
-    "@ionic/core/css/normalize.css",
-    "@ionic/core/css/structure.css",
-    "@ionic/core/css/typography.css",
-    "@ionic/core/css/ionic.bundle.css",
-    "@/assets/css/main.css",
-  ],
-});
+  colorMode: {
+      classSuffix: ''
+  },
+  apollo: {
+      clients: {
+          default: {
+              httpEndpoint: 'https://rickandmortyapi.com/graphql'
+          }
+      },
+  },
+})
